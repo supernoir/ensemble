66338d6735157167325bd22eaad368502bee4a09
supernoir <kawohl.bentodesign@gmail.com>
2016-02-14 18:25:42 +0100

    adds config, mongoose models

--- app/server.js
+++ app/server.js
@@ -1,3 +1,7 @@
+// -----------------------------------------------------------------------------  
+//  SETUP
+// -----------------------------------------------------------------------------
+
 var connect = require('connect');
 var http = require('http');
 var express  = require('express');
@@ -5,11 +9,44 @@ var bodyParser = require('body-parser');
 var cors = require("cors");
 var morgan = require('morgan');
 var mongoose = require('mongoose');   
-
 var app = express();
 
+// -----------------------------------------------------------------------------  
+//  CONFIGURATION
+// -----------------------------------------------------------------------------
 
+mongoose.connect('mongodb://localhost/library');
+app.use(express.static(__dirname + '/app'));         
 
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(function (request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    response.header("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS");  
+  next();      
+});
+
+// -----------------------------------------------------------------------------  
+//  MONGOOSE MODELS
+// -----------------------------------------------------------------------------
+
+var Characters = mongoose.model('Characters', {
+    firstname : String,
+    middlename : String,
+    lastname : String, 
+    gender : String,
+    origin : String,
+    birthday : Date
+});
+
+// -----------------------------------------------------------------------------  
+//  LISTENING
+// -----------------------------------------------------------------------------
 
 var port = 3000;
 app.listen(port);
