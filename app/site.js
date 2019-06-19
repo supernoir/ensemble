d3d3f537b391a51ab330b1c69430f7ddfde6e973
supernoir <kawohl.bentodesign@gmail.com>
2019-06-19 18:46:23 +0200

    reformats serviceworker files

--- app/site.js
+++ app/site.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-   navigator.serviceWorker.register('sw.js')
-    .then(console.log('SW registered'))
-    .catch(console.error('An error occurred while registering the SW'));
+	navigator.serviceWorker.register('sw.js')
+		.then(console.log('SW registered'))
+		.catch(console.error('An error occurred while registering the SW'));
 }
--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
 self.addEventListener('install', function (event){
-  console.log('SW installed');
-  // Install and Activate right away
-  self.skipWaiting();
+	console.log('SW installed');
+	// Install and Activate right away
+	self.skipWaiting();
 });
 
 self.addEventListener('activate', function (event){
-  console.log('SW activated');
+	console.log('SW activated');
 });
 
 self.addEventListener('fetch', function (event) {
-  console.log(event.request.url);
+	console.log(event.request.url);
 
-  if (!navigator.onLine) {
-    event.respondWith(new Response('<h1> Offline :( </h1>', {headers: { 'Content-Type': 'text/html'}}));
-  } else {
-    // Pass through request
-    event.respondWith(fetch(event.request));
-  }
-})
+	if (!navigator.onLine) {
+		event.respondWith(new Response('<h1> Offline :( </h1>', { headers: { 'Content-Type': 'text/html' } }));
+	} else {
+		// Pass through request
+		event.respondWith(fetch(event.request));
+	}
+});
