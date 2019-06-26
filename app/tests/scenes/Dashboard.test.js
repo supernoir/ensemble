83780e5c380b944d61bc0270edb3c094f94b67aa
supernoir <kawohl.bentodesign@gmail.com>
2019-06-26 19:16:22 +0200

    sets up jest testing for frontend

---
+++ app/tests/scenes/Dashboard.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+// Import Components
+import Dashboard from './../../js/pages/Dashboard';
+
+// Import Mock Data
+//import * as mockData from './mocks/mockApiResponse.json';
+
+test('Dashboard -- Snapshot Test', () => {
+	const wrapper = shallow(<Dashboard getEvents={() => true} />);
+	expect(wrapper).toMatchSnapshot();
+});
+
+/* describe('Dashboard', function() {
+	it('should render a loader', function() {
+		expect(shallow(<Dashboard data={[{ data: mockData }]} loading={true} />).contains(<Loader />)).toBe(true);
+	});
+}); */
